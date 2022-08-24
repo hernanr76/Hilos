cd4d4f49f5e670fbdcc7bce58a40cf417aa631e9
Hernán <hrodriguez@aconcaguasf.com>
2022-08-24 16:03:57 -0300

    Add project files.

---
+++ MyClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hilos
+{
+    public class MyClass
+    {
+        int _seconds;
+        public delegate void Seconds(int n);
+        public event Seconds? SecondsChanged;
+
+        public MyClass()
+        {
+            Console.WriteLine($"Child Thread: {Environment.CurrentManagedThreadId}");
+            _seconds = 0;
+        }
+
+        public async Task DoSomething() 
+        { 
+            do
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                _seconds++;
+                OnSecondsChanged();
+
+            } while (true);
+        }
+
+        protected virtual void OnSecondsChanged()
+        {
+            SecondsChanged?.Invoke(_seconds);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Hilos;
+
+Console.WriteLine($"Main Thread: {Environment.CurrentManagedThreadId}");
+
+//Creo una nueva tarea que va a ejecutar la función asincrónica DoSomething
+var t = Task.Factory.StartNew(() => { _ = DoSomething(); });
+var rnd = new Random();
+
+while (true)
+{
+    Console.Write(".");
+    Thread.Sleep(200);
+}
+
+//Evento
+static void EventSeconds(int n)
+{
+    Console.WriteLine($"{n} segundos");
+}
+
+//Función asincrónica
+static async Task<MyClass>DoSomething()
+{
+    var c = new MyClass();
+    //Registro el eveno
+    c.SecondsChanged += EventSeconds;
+    await c.DoSomething();
+    return c;
+}
+
